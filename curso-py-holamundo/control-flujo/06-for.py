a4d299a3a040d6852c205e4c42d0786d037a51e8
CL-CDOMINGUEZB\Carlos Dominguez <cdominguez@ansuz.la>
2025-05-24 17:07:26 -0400

    seccion control de flujo completa

---
+++ curso-py-holamundo/control-flujo/06-for.py
@@ -0,0 +1,11 @@
+buscar = 10
+
+for numero in range(5):
+    print(numero)
+    if numero == buscar:
+        print("encontrado", buscar)
+        break
+else:
+    print("no encontré el número buscado :(")
+
+# tambien se pueden iterar los srings
